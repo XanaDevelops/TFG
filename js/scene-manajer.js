5a2160010ae0e73c4e06df9d77a06d9ff5f05ab4
Daniel GV <danielgv.space@gmail.com>
2026-05-12 19:26:52 +0200

    prueba de concepto escenas

---
+++ js/scene-manajer.js
@@ -0,0 +1,41 @@
+AFRAME.registerComponent('scene-manager', {
+    schema: {
+
+    },
+
+    init: function () {
+        // Do something when component first attached.
+        this.scenes = ["scenes/mainMenu.html",
+            "scenes/mainScene.html"
+        ]
+        this.currentScene = 0
+
+        this.loadScene(this.currentScene)
+    },
+
+    loadScene: function (index) {
+        this.currentScene = index
+        this.el.setAttribute("template", { "src": this.scenes[this.currentScene] })
+    },
+
+    update: function () {
+        // Do something when component's data is updated.
+    },
+
+    remove: function () {
+        // Do something the component or its entity is detached.
+    },
+
+    tick: function (time, timeDelta) {
+        // Do something on every scene tick or frame.
+    }
+});
+
+
+AFRAME.registerComponent("borrar", {
+    init: function () {
+        setTimeout(() => {
+            document.getElementById("sceneContainer").components["scene-manager"].loadScene(1)
+        }, 2500);
+    }
+})
