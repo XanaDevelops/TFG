712a3ee71c8d8ea9b9c58be982e4c9aa99cc3a6f
XanaDevelops <danielgv.space@gmail.com>
2026-06-09 00:57:21 +0200

    proyector pilla indices activos
    Signed-off-by: XanaDevelops <danielgv.space@gmail.com>

--- js/projector-controller.js
+++ js/projector-controller.js
@@ -6,6 +6,12 @@ AFRAME.registerComponent('projector-controller',{
         this.lights = document.getElementById("shadowLights")
 
 
+        this.loadFromTemplates = () => {
+            // mantener orden!
+            this.selectors = ["Perfil", "Planta", "Alzada"].map((x) => {
+                return document.getElementById("btnShadow"+x).components['shadow-selector'].currentIndex})
+        }
+        
         //estado inicial
         this.lights.setAttribute('visible', false)
 
@@ -13,6 +19,20 @@ AFRAME.registerComponent('projector-controller',{
             this.lights.setAttribute('visible', !this.lights.getAttribute('visible'))
         }
 
+        this.validate = () => {
+            // por template recargar cada vez...
+            this.loadFromTemplates()
+            console.log(this.selectors);
+
+            
+        }
+
         this.el.addEventListener('toggle-light', this.toggleLight)
+        this.el.addEventListener('validate-shadows', this.validate)
+    },
+
+    // TODO: :D
+    validate : function(obj, alz, pla, per) {
+        return true
     }
 })
