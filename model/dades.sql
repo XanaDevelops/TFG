76999047bc268f7355d7b57073ff0fbcc3dd14e4
XanaDevelops <danielgv.space@gmail.com>
2026-06-17 17:31:23 +0200

    totest implementado SQL

--- model/dades.sql
+++ model/dades.sql
@@ -1,1 +1,12 @@
 USE FIGURES;
+
+-- Insert shadows
+INSERT INTO OMBRA (meshID, imgID, nom) VALUES
+('foratCercle', 'shadowCircle', 'Cercle'),
+('foratQuadrat', 'shadowSquare', 'Quadrat'),
+('foratEquilater', 'shadowTriangle', 'Equilàter');
+
+-- Insert FIGURA triforma with references to the shadows
+-- Assuming the shadows get IDs 1, 2, 3 in order of insertion
+INSERT INTO FIGURA (meshID, nom, descript, alzada, planta, perfil) VALUES
+('triforma', 'Triforma', 'Combinació de les tres formes geomètriques', 1, 3, 2);
