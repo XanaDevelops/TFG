16cd5b684630c2e6599e08a808280ea18f67d856
XanaDevelops <danielgv.space@gmail.com>
2026-06-26 16:27:20 +0200

    configuración portatil

---
+++ deploy_APACHE.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SERVER_PATH="/var/www/html/"
+
+#eval "$(ssh-agent -s)"
+#echo "$SERVER_PATH"
+#git pull
+
+sudo -S rsync -a --delete --mkpath \
+	--include='/img/***' \
+	--include='/files/***' \
+	--include='/fonts/***' \
+	--include='/js/***' \
+	--include='/snd/***' \
+	--include='/index.html' \
+	--include='/backend.php' \
+	--include='/scenes/***' \
+	--include='/templates/***' \
+	--exclude='*' \
+	./ "$SERVER_PATH/"
+
+if [ $? -eq 0 ]; then
+	echo "rsync ok"
+else
+	echo "rsync err"
+fi
+
+
