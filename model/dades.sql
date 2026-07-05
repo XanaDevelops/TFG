871605ccb1315ad161840baa7d970da417c71a71
Daniel GV <danielgv.space@gmail.com>
2026-07-05 23:01:35 +0200

    update db

--- model/dades.sql
+++ model/dades.sql
@@ -7,7 +7,9 @@ INSERT INTO OMBRA (id, nom, meshID, imgID) VALUES
 (2, 'Quadrat', '#foratQuadrat', '#shadowSquare'),
 (3, 'Equilàter', '#foratEquilater', '#shadowTriangle'),
 (4, '#T', '#T', '#shadowT'), 
-(5, '#V', '#V', '#shadowV'); 
+(5, '#V', '#V', '#shadowV'),
+(6, 'Rectangle', '#foratRectangule', '#shadowRect'); 
+(7, 'Plus', '#foratPlus', '#shadowPlus'); 
 
 -- Insert CLASSE records (0-indexed)
 INSERT INTO CLASSE (id, nom, descripcio) VALUES
@@ -26,9 +28,9 @@ INSERT INTO FIGURA (id, meshID, nom, descript, alzada, planta, perfil) VALUES
 (3, '#tetra', 'Tetrahedron', 'Forma bàsica tetraèdrica', 3, 3, 3),
 (4, '#piramid', 'Pyramid', 'Forma piramidal', 3, 2, 3),
 (5, '#cone', 'Cone', 'Forma cònica', 3, 1, 3),
-(6, '#prism', 'Prism', 'Forma prismàtica', 0, 2, 0),
+(6, '#prism', 'Prism', 'Forma prismàtica', 2, 2, 2),
 (7, '#cylinder', 'Cylinder', 'Forma cilíndrica', 2, 1, 2),
-(8, '#plus', 'Plus', 'Figura amb forma de plus', 0, 2, 2),
+(8, '#plus', 'Plus', 'Figura amb forma de plus', 7, 2, 2),
 (9, '#L', 'L', 'Figura amb forma de L', 0, 2, 2), 
 (10, '#T', 'T', 'Figura amb forma de T', 4, 2, 2),
 (11, '#V', 'V', 'Figura amb forma de V', 5, 2, 2);
